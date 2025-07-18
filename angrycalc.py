beaffea7171d4b49b43bf14e4c4bfa9021ee8b74
Alanreet <ethanlilly123@aol.com>
2025-07-18 19:21:06 -0400

    Add files via upload

---
+++ angrycalc.py
@@ -0,0 +1,9 @@
+print("fuck you this is my calculator")
+while True:
+    problem = input("enter your problem asshole: ")
+    solution = eval(problem)
+    print("the answer is, " +str(solution))
+    again = input("Do you have another problem (yes/no): ")
+    if again.lower() == "no":
+        print("fuck off and kill yourself")
+        break
